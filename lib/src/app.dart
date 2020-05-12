14d4b9d6b483b7100a23656e51aec893d0ec6a20
luisvalle <lvallep@indracompany.com>
2020-05-12 15:12:18 -0500

    separacion de logica

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hola_mundo/src/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text("Hola Mundo 02")));
+    return MaterialApp(home: HomePage());
   }
 }
